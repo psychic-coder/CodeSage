f8e5b59712bf48021fcd6840ca2db097e2eeeb0c
Rohit Ganguly <rohitganguly450@gmail.com>
2026-05-25 14:47:37 +0530

    Add styles.d.ts declarations for CSS and images

---
+++ frontend/types/styles.d.ts
@@ -0,0 +1,15 @@
+declare module '*.css';
+declare module '*.module.css';
+declare module '*.scss';
+declare module '*.module.scss';
+declare module '*.less';
+declare module '*.module.less';
+
+// Allow importing images as modules in TS
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.jpeg';
+declare module '*.gif';
+declare module '*.svg';
+
+export {};
